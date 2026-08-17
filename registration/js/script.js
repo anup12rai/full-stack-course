d4a68ff25bdc6d6cccddff9086b76c6dfbff8f0b
anup12rai <aniruddharai39@gmail.com>
2026-08-17 08:13:52 +0545

    eskhgr
    wekrhu

---
+++ registration/js/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    // Simple login validation
+    if (username === "admin" && password === "1234") {
+        message.style.color = "green";
+        message.textContent = "Login Successful!";
+    } else {
+        message.style.color = "red";
+        message.textContent = "Invalid Username or Password!";
+    }
+});
